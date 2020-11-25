7ba6f6d576d4ed4157a04f40fc1a464ce5b166e6
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-25 15:57:12 -0300

    Exemplo 6 praticando testes parte 2

--- Java_script_ES6_testes_unitarios/exemplo_06_praticando.js
+++ Java_script_ES6_testes_unitarios/exemplo_06_praticando.js
@@ -1,9 +1,18 @@
 const assert = require('assert');
-const { constants } = require('buffer');
 
+//Exemplo com strings
 const welcome = (name) => { return`Ola ${name} seja bem-vindo!`};
 
 const expected = welcome('Cleberton');
 
 assert.strictEqual(expected, 'Ola Cleberton seja bem-vindo!', 'Erro na saida');
 
+//exemplo com funçoes
+
+function divisao(num1, num2){
+    return num1 / num2;
+}
+
+const expected2 = divisao(10,2);
+
+assert.strictEqual(expected2, 5, 'Esperado resultado de 5! divisão de 10 por 2.' );
